a31531524c71a1bc897ce1f2b9459a157b0ba416
Anatoly <waveinspectro@gmail.com>
2026-02-04 20:59:20 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/tennis_detector/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tennis_detector/MainActivity.kt
@@ -286,17 +286,23 @@ class MainActivity: FlutterActivity() {
         return count
     }
 
-    // Извлечение боксов из выхода модели (YOLOv8: x, y, w, h)
-    private fun extractBoxesFromOutput(output: Array<Array<FloatArray>>): List<List<Double>> {
-        val boxes = mutableListOf<List<Double>>()
+    // Извлечение боксов из выхода модели (YOLOv8: x, y, w, h - нормализованные 0-1)
+    private fun extractBoxesFromOutput(output: Array<Array<FloatArray>>): List<Map<String, Double>> {
+        val boxes = mutableListOf<Map<String, Double>>()
         for (i in 0 until 8400) {
-            val conf = output[0][4][i]
-            if (conf > 0.3f) {
-                val x = output[0][0][i]
-                val y = output[0][1][i]
-                val w = output[0][2][i]
-                val h = output[0][3][i]
-                boxes.add(listOf(x.toDouble(), y.toDouble(), w.toDouble(), h.toDouble()))
+            val conf = output[0][4][i].toDouble()
+            if (conf > 0.3) {
+                val x = (output[0][0][i] / 640.0).toDouble()
+                val y = (output[0][1][i] / 640.0).toDouble()
+                val w = (output[0][2][i] / 640.0).toDouble()
+                val h = (output[0][3][i] / 640.0).toDouble()
+                boxes.add(mapOf(
+                    "x" to x,
+                    "y" to y,
+                    "w" to w,
+                    "h" to h,
+                    "conf" to conf
+                ))
             }
         }
         return boxes
